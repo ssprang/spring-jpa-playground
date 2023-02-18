188c70d878c3101cfd3abe71141f8dbf3689ffad
Sara Språng <sara.sprang@epidemicsound.com>
2023-02-18 16:47:12 +0100

    Initial commit.

---
+++ src/test/kotlin/com/example/springjpaplayground/SpringJpaPlaygroundApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.springjpaplayground
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringJpaPlaygroundApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
